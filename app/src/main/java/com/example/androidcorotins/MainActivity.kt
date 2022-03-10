c84e2b5adb020e16f9050d619d0a13b97279fa1b
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-03-10 13:32:22 +0600

    updated

--- app/src/main/java/com/example/androidcorotins/MainActivity.kt
+++ app/src/main/java/com/example/androidcorotins/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.*
+import kotlin.concurrent.thread
 import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonOne:Button
     private lateinit var buttonTwo:Button
     private  var zero = 0
+//    private lateinit var thread :Thread ;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,13 +37,76 @@ class MainActivity : AppCompatActivity() {
             Log.d("thread", "${Thread.currentThread().name}");
         }
 
-        //decrement
+        //decrement and useing coroutine all scope
         buttonTwo.setOnClickListener{
             if(zero == 0){}else{
                 zero--
                 zeroTv.setText(""+zero);
             }
-            Log.d("thread", "${Thread.currentThread().name}");
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.d("thread", "1-${Thread.currentThread().name}");
+            }
+
+            GlobalScope.launch(Dispatchers.Main){
+                Log.d("thread", "2-${Thread.currentThread().name}");
+            }
+
+            MainScope().launch(Dispatchers.Default){
+                Log.d("thread", "3-${Thread.currentThread().name}");
+            }
+
+        }
+
+//        forloop(10);
+
+        CoroutineScope(Dispatchers.Main).launch {
+//            Task1();
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+//            Task2()
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            printFollowers()
+        }
+
+    }
+
+    suspend fun printFollowers(){
+        var follower = 0
+      var job =  CoroutineScope(Dispatchers.IO).launch {
+           follower = getFollowers()
+       }
+        job.join()
+        println(follower)
+    }
+
+    suspend fun getFollowers():Int{
+        delay(1000)
+        return 54;
+
+    }
+
+    suspend fun Task1(){
+        println("start task 1")
+        yield()
+        println("end task 1")
+    }
+
+    suspend fun Task2(){
+        println("start task 2")
+        yield()
+        println("end task 2")
+    }
+
+
+    fun forloop(n:Int){ //here i using java thread function
+
+        thread(start= true){
+            for(i in 1..n){
+                println("printing")
+            }
         }
 
     }
